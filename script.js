64dc7f5c499993eed9d65473f09195678a1183e3
solostyle <spam@solostyle.net>
2014-08-25 14:58:34 -0500

    use reverse with data-binding

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function FirstCtrl($scope, Data) {
 function SecondCtrl($scope, Data) {
   $scope.data = Data;
 
-  $scope.reversedMessage = function () {
-    return $scope.data.message.split("").reverse().join();
+  $scope.reversedMessage = function (message) {
+    return message.split("").reverse().join("");
   };
 };
 
