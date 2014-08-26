e99ccc625755f5f61bf1ee66ed34a79539e090ee
solostyle <spam@solostyle.net>
2014-08-26 14:36:28 -0500

    filter trial

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function FirstCtrl($scope) {
-  //$scope.data = Data;
+"use strict";
+var myApp = angular.module('myApp',[]);
+myApp.factory('Data', function () {
+  return { message: "I'm data from a service" };
+});
+
+myApp.filter('reverse', function () {
+  return function (text) {
+    return text.split("").reverse().join("");
+  }
+});
+
+function FirstCtrl($scope, Data) {
+  $scope.data = Data;
 };
 
-function SecondCtrl($scope) {
-  //$scope.data = Data;
+function SecondCtrl($scope, Data) {
+  $scope.data = Data;
 };
 
