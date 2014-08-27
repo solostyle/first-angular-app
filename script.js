d50da908021a7e8c30a15e5805ec15f3ed64b40d
solostyle <spam@solostyle.net>
2014-08-27 11:00:30 -0500

    now if a chore is typed in and im done is clicked, still undefined

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var app = angular.module("choreApp", []);
+app.controller("choreCtrl", function ($scope) {
+  $scope.logChore = function (chore) {
+    alert(chore + " is done!");
+  };
+});
 
 app.directive("kid", function () {
   return {
     restrict: "E",
-    scope: {},
-    template: '<input type="text" ng-model="chore">{{chore}}'
+    scope: {
+      done: "&"
+    },
+    template: '<input type="text" ng-model="chore">' +
+      '{{chore}}' +
+      '<div class="button" ng-click="done()">I\'m done!</div>'
   }
 });
 
