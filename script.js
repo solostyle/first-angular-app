39ea37245fec4aeb6d9cc1fa224ef3d253a81abc
solostyle <spam@solostyle.net>
2014-08-27 09:34:55 -0500

    restrict to comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var app = angular.module("superhero", []);
 
 app.directive("superman", function () {
   return {
-    restrict: "C",
+    restrict: "M",
     link: function () {
       alert("I'm working");
     }
