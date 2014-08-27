1795105d47137cfbb93f8984c4e47ef8d2847762
solostyle <spam@solostyle.net>
2014-08-27 09:09:43 -0500

    extract classname from directive into attribute of directive

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var app = angular.module("behaviorApp", []);
 
 app.directive("enter", function () {
-  return function (scope, element) {
+  return function (scope, element, attrs) {
     element.bind("mouseenter", function () {
-      element.addClass("panel"); 
+      element.addClass(attrs.enter); 
     });
   }
 });
 
 app.directive("leave", function () {
-  return function (scope, element) {
+  return function (scope, element, attrs) {
     element.bind("mouseleave", function () {
-      element.removeClass("panel");
+      element.removeClass(attrs.leave);
     });
   }
 });
