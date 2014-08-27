e8aea713607fe20e0304d8a2b90f765fd51717be
solostyle <spam@solostyle.net>
2014-08-27 09:30:51 -0500

    restrict to attribute

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var app = angular.module("superhero", []);
 
 app.directive("superman", function () {
   return {
-    restrict: "E",
-    template: "<div>Here I am to save the day</div>"
+    restrict: "A",
+    link: function () {
+      alert("I'm working");
+    }
   }
 });
