c2cb2d940a93f8e402d32ad04e869ae4898443f0
solostyle <spam@solostyle.net>
2014-08-27 09:05:42 -0500

    using mouse behaviors to change style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var app = angular.module("behaviorApp", []);
 app.directive("enter", function () {
   return function (scope, element) {
     element.bind("mouseenter", function () {
-      console.log("I'm inside of you!");
+      element.addClass("panel"); 
     });
   }
 });
@@ -11,7 +11,7 @@ app.directive("enter", function () {
 app.directive("leave", function () {
   return function (scope, element) {
     element.bind("mouseleave", function () {
-      console.log("I'm leaving on a jet plane!");
+      element.removeClass("panel");
     });
   }
 });
