8c3a2a4d98fb1fadb24b6a34ef3365a622006650
solostyle <spam@solostyle.net>
2014-08-27 10:36:53 -0500

    define scope property to isolate scope

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var app = angular.module("choreApp", []);
 app.directive("kid", function () {
   return {
     restrict: "E",
+    scope: {},
     template: '<input type="text" ng-model="chore">{{chore}}'
   }
 });
