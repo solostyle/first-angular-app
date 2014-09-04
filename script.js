dcebf4b0cf89c32c524da537e20d26bba8ed5567
solostyle <spam@solostyle.net>
2014-09-04 09:08:32 -0500

    isolate-scope: review

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-var app = angular.module("drinkApp", []);
+var app = angular.module("phoneApp", []);
 app.controller("AppCtrl", function ($scope) {
-  $scope.ctrlFlavor = 'blackberry';
-});
-
-app.directive("drink", function () {
-  return {
-    scope: {
-      flavor: "="
-    },
-    template: '<input type="text" ng-model="flavor">',
+  $scope.leaveVoicemail = function (number, message) {
+    alert("Number: " + number + " said: " + message);
   }
-});
+})
+app.directive("phone", function () {
+  return {
+    restrict: "E",
+    scope: {
+      number: "@",
+      network: "=",
+      makeCall: "&"
+    },
+    template: '<div class="panel">Number: {{number}} Network:<select ng-model="network" ng-options="net for net in networks">' +
+        '<input type="text" ng-model="value">' +
+        '<div class="button" ng-click="makeCall({number: number, message: value})">Call home!</div></div>',
+    link: function (scope) {
+      scope.networks = ["Verizon", "AT&T", "Sprint"];
+      scope.network = scope.networks[0];
+    }
+  }
+})
